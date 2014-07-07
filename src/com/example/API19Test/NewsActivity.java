7ca77ad768233a1e93b3bffdadbbbbcfe73707e4
Amila Dissanayake <amilad@hsenidmobile.com>
2014-07-07 09:27:03 +0530

    added news view

--- src/com/example/API19Test/NewsActivity.java
+++ src/com/example/API19Test/NewsActivity.java
@@ -1,11 +1,25 @@
 package com.example.API19Test;
 
-import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class NewsActivity extends Activity{
+public class NewsActivity extends ListActivity{
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.news_layout);
+
+        // storing string resources into Array
+        String[] adobe_products = getResources().getStringArray(R.array.adobe_products);
+
+        // Binding resources Array to ListAdapter
+        this.setListAdapter(new ArrayAdapter<String>(this, R.layout.news_layout, R.id.label, adobe_products));
+
     }
 }
