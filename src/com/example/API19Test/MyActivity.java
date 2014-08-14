422acb2d3f6da817fe0cdf003be6df187b9e9c35
Amila Dissanayake <amilad@hsenidmobile.com>
2014-08-14 12:16:16 +0530

    completed news tab

--- src/com/example/API19Test/MyActivity.java
+++ src/com/example/API19Test/MyActivity.java
@@ -5,76 +5,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
-import android.widget.Toast;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URI;
-import java.net.URL;
 
 public class MyActivity extends TabActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        System.out.println("$$$$$$$$$$$$$$ Hello");
-        String id;
-        String name;
-        InputStream is=null;
-        String result=null;
-        String line=null;
-        try
-        {
-            HttpClient httpclient = new DefaultHttpClient();
-            HttpPost httppost = new HttpPost("http://127.0.0.1/login.php");
-//            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-            HttpResponse response = httpclient.execute(httppost);
-            HttpEntity entity = response.getEntity();
-            is = entity.getContent();
-        }
-        catch(Exception e)
-        {
-            Toast.makeText(getApplicationContext(), "Invalid IP Address",
-                    Toast.LENGTH_LONG).show();
-        }
-
-        try
-        {
-            BufferedReader reader = new BufferedReader
-                    (new InputStreamReader(is,"iso-8859-1"),8);
-            StringBuilder sb = new StringBuilder();
-            while ((line = reader.readLine()) != null)
-            {
-                System.out.println("$$$$$$$$$$"+line);
-                sb.append(line + "\n");
-            }
-            is.close();
-            result = sb.toString();
-        }
-        catch(Exception e)
-        {
-        }
-
-        try
-        {
-            JSONObject json_data = new JSONObject(result);
-            name=(json_data.getString("name"));
-            Toast.makeText(getBaseContext(), "Name : "+name,
-                    Toast.LENGTH_SHORT).show();
-        }
-        catch(Exception e)
-        {
-        }
-        /*TabHost tabHost = getTabHost();
+        TabHost tabHost = getTabHost();
 
         // Tab for Photos
         TabSpec newsSpec = tabHost.newTabSpec("News");
@@ -91,6 +28,6 @@ public class MyActivity extends TabActivity {
 
         // Adding all TabSpec to TabHost
         tabHost.addTab(newsSpec); // Adding photos tab
-        tabHost.addTab(videospec); // Adding videos tab*/
+        tabHost.addTab(videospec); // Adding videos tab
     }
 }
--- src/com/example/API19Test/NewsActivity.java
+++ src/com/example/API19Test/NewsActivity.java
@@ -1,25 +1,74 @@
 package com.example.API19Test;
 
 import android.app.ListActivity;
-import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
+import android.util.Log;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
 
-public class NewsActivity extends ListActivity{
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NewsActivity extends ListActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AsynTask asynTask = new AsynTask();
+        asynTask.execute();
 
-        // storing string resources into Array
-        String[] adobe_products = getResources().getStringArray(R.array.adobe_products);
+    }
 
-        // Binding resources Array to ListAdapter
-        this.setListAdapter(new ArrayAdapter<String>(this, R.layout.news_layout, R.id.label, adobe_products));
+    public void setAdapterData(List<String> list) {
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.news_layout, R.id.label, list));
+    }
 
+    private class AsynTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            InputStream is = null;
+            try {
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpPost httppost = new HttpPost("http://192.168.0.118/login.php");
+                HttpResponse response = httpclient.execute(httppost);
+                HttpEntity entity = response.getEntity();
+                is = entity.getContent();
+            } catch (Exception e) {
+                Log.e("log_tag", "Error in http connection" + e.toString());
+            }
+
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 8);
+                String line = reader.readLine();
+                JsonParser parser = new JsonParser();
+                JsonArray jArray = parser.parse(line).getAsJsonArray();
+                final List<String> newsList = new ArrayList<String>();
+                for (JsonElement obj : jArray) {
+                    newsList.add(obj.getAsJsonObject().get("description").toString());
+                }
+                runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        setAdapterData(newsList);
+
+                    }
+                });
+                is.close();
+            } catch (Exception e) {
+                Log.e("log_tag", "Error converting result " + e.toString());
+            }
+            return null;
+        }
     }
 }
