9d076d5d6ad217898616731d60228c265e61a536
Amila Dissanayake <amilad@hsenidmobile.com>
2014-10-31 13:54:39 +0530

    added video support

--- src/com/example/API19Test/VideoActivity.java
+++ src/com/example/API19Test/VideoActivity.java
@@ -7,17 +7,23 @@ import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class VideoActivity extends Activity {
-    String SrcPath = "rtsp://v5.cache1.c.youtube.com/CjYLENy73wIaLQnhycnrJQ8qmRMYESARFEIJbXYtZ29vZ2xlSARSBXdhdGNoYPj_hYjnq6uUTQw=/0/0/0/video.3gp";
+    String SrcPath = "http://192.168.0.118/AndroidCommercial.3gp";
 
-    /** Called when the activity is first created. */
+    /**
+     * Called when the activity is first created.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.video_layout);
-        VideoView myVideoView = (VideoView)findViewById(R.id.myvideoview);
-        myVideoView.setVideoURI(Uri.parse(SrcPath));
-        myVideoView.setMediaController(new MediaController(this));
-        myVideoView.requestFocus();
-        myVideoView.start();
+        try {
+            VideoView myVideoView = (VideoView) findViewById(R.id.myvideoview);
+            myVideoView.setVideoURI(Uri.parse(SrcPath));
+            myVideoView.setMediaController(new MediaController(this));
+            myVideoView.requestFocus();
+            myVideoView.start();
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
     }
 }
